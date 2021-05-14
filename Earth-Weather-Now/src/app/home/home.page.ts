79fb1c2f37c279fd56623e9ea539408aa40e5a08
abishekanthony07 <abishekanthony07@gmail.com>
2021-05-14 16:48:16 +0200

    EWN_4 - Erweiterte Suche wird ein und ausgeblendet, 2-Way-Binding variablen hinzugefügt

--- Earth-Weather-Now/src/app/home/home.page.ts
+++ Earth-Weather-Now/src/app/home/home.page.ts
@@ -8,11 +8,29 @@ import { HttpClient, HttpErrorResponse } from '@angular/common/http'
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  private erweiterteSuche: boolean;
+  private city: string;
+  private stateCode: string;
+  private laenderCode: string;
 
-  constructor(private httpClient: HttpClient) {}
+  constructor(private httpClient: HttpClient) {
+    this.erweiterteSuche = false
+  }
 
   async getData() {
-    new WeatherService(this.httpClient).getData("10117", "", "")
+    new WeatherService(this.httpClient).getData(this.city, this.stateCode, this.laenderCode);
+  }
+
+  async onErweitert(){
+    this.erweiterteSuche = !this.erweiterteSuche;
+    this.resetErweitertValuesIfHidden()
+  }
+
+  private resetErweitertValuesIfHidden(){
+    if(!this.erweiterteSuche){
+      this.stateCode = ""
+      this.laenderCode = ""
+    }
   }
 
 }
