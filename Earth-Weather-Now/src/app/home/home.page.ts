24737bfe8e069e286552e34c9152ab1c9b3c5129
abishekanthony07 <abishekanthony07@gmail.com>
2021-05-14 18:06:23 +0200

    EWN_5 - Die Information können (auch für immer) ausgeblendet werden

--- Earth-Weather-Now/src/app/home/home.page.ts
+++ Earth-Weather-Now/src/app/home/home.page.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 import { WeatherService } from '../api/weather.service';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http'
 
+import { Storage } from '@ionic/storage';
+import { SpeicherService } from '../speicher.service';
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -12,15 +15,30 @@ export class HomePage {
   private city: string;
   private stateCode: string;
   private laenderCode: string;
+  private showHomeInformation: boolean = false;
+  private closeInformation: boolean = false
 
-  constructor(private httpClient: HttpClient) {
+  constructor(private httpClient: HttpClient, private speicherservice: SpeicherService) {
     this.erweiterteSuche = false
+    this.loadHideInformation()
   }
 
   async getData() {
     new WeatherService(this.httpClient).getData(this.city, this.stateCode, this.laenderCode);
   }
 
+  /**Information functions */
+  async loadHideInformation(){
+    this.showHomeInformation = await this.speicherservice.getHideInformation()
+    this.closeInformation = this.showHomeInformation
+  }
+
+  async closeButtonPressed() {
+    this.closeInformation = true
+    this.speicherservice.setHideInformation(this.showHomeInformation)
+  }
+
+  /** Toolbar functions */
   async onErweitert(){
     this.erweiterteSuche = !this.erweiterteSuche;
     this.resetErweitertValuesIfHidden()
---
+++ Earth-Weather-Now/src/app/speicher.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { Storage } from '@ionic/storage';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SpeicherService {
+
+  constructor(private storage: Storage) {
+
+  }
+
+  setHideInformation(value: boolean){
+    this.storage.set('showHomeInformation', value);
+  }
+
+  getHideInformation(): Promise<boolean> {
+    return this.storage.get('showHomeInformation')
+  }
+}
