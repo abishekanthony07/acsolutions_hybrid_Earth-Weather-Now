249cbcc6df3f4f3b4a8a93584212d8e2344e3522
Abishek Anthony <abishekanthony07@gmail.com>
2021-05-24 14:38:04 +0200

    EWN_9 - Menü wird angezeigt

--- Earth-Weather-Now/src/app/app.component.ts
+++ Earth-Weather-Now/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import { MenuController } from '@ionic/angular';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,33 @@ import {Component} from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  public selectedIndex = 0;
+  public appPages = [
+    {
+      title: 'Wetter',
+      url: '/folder/Startseite',
+      icon: 'sun-outline'
+    },
+  ];
+
+  constructor(
+    private menuController: MenuController
+  ) {
+    this.initializeApp();
+  }
+
+  initializeApp() {
+  }
+
+  menuSchliessen(){
+    this.menuController.close();
+  }
+
+  // eslint-disable-next-line @angular-eslint/use-lifecycle-interface
+  ngOnInit() {
+    const path = window.location.pathname.split('folder/')[1];
+    if (path !== undefined) {
+      this.selectedIndex = this.appPages.findIndex(page => page.title.toLowerCase() === path.toLowerCase());
+    }
+  }
 }
